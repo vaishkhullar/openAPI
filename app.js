4cb2311ae577507f9118c15dffd26ceccd057392
vaish.khullar <vaishnavikhullar@hotmail.com>
2022-12-16 11:15:48 +0000

    added in the additional tests

--- app.js
+++ app.js
@@ -23,8 +23,14 @@ app.use((err, req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  if ((err.code === "22P02") | (err.code === "23503")) {
+  if (err.code === "22P02") {
+    // type is different
     res.status(400).send({ msg: "bad request" });
+  } else if (err.code === "23503") {
+    // this is missing foreign key
+    res.status(404).send({ msg: "not found" });
+  } else if (err.code === "23502") {
+    res.status(400).send({ msg: "missing required field(s)" });
   }
 });
 
